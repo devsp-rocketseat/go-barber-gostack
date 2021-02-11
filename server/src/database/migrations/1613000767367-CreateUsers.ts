115fe8fb0f3211c0edd89739d7a02b24f6a9a9e9
DevSP <contatodevsp@gmail.com>
2021-02-10 21:32:06 -0300

    Relacionamento nos models

--- server/src/database/migrations/1613000767367-CreateUsers.ts
+++ server/src/database/migrations/1613000767367-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1613000767367 implements MigrationInterface {
                 columns: [
                     {
                         name: 'id',
-                        type: 'varchar',
+                        type: 'uuid',
                         isPrimary: true,
                         generationStrategy: 'uuid',
                         default: 'uuid_generate_v4()',
