5ce9681569e381925e7fdfa336d6a72b161654ba
DevSP <contatodevsp@gmail.com>
2021-02-11 21:27:12 -0300

    Gerando token JWT

--- server/src/services/AuthenticateUserService.ts
+++ server/src/services/AuthenticateUserService.ts
@@ -1,5 +1,7 @@
 import { getRepository } from 'typeorm'
 import { compare } from 'bcryptjs'
+import { sign } from 'jsonwebtoken'
+
 
 import User from '../models/User'
 
@@ -10,6 +12,7 @@ interface Request {
 
 interface Response {
     user: User
+    token: string
 }
 
 class AuthenticateUserService {
@@ -28,8 +31,14 @@ class AuthenticateUserService {
             throw Error('Incorrect email/password combination')
         }
 
+        const token = sign({}, '5cd8d3355eea2c2662a74f4154758325', {
+            subject: user.id,
+            expiresIn: '1d'
+        })
+
         return {
             user,
+            token
         }
     }
 }
